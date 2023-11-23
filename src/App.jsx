b9801b008ecd9bafdd4847101f0f25ad4f3813c5
Ellie <eagyeman1234@gmail.com>
2023-11-23 12:05:13 -0500

    add form and button for to do list in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+import "./styles.css"
 
 function App() {
- return "Hi"
+  return <form className="new-item-form">
+    <div className="form-row">
+      <label htmlFor="item">New Item</label>
+      <input type="text" name="" id="item" />
+    </div>
+    <button className="btn">Add</button>
+  </form>
 }
 
 
