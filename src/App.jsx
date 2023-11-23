8a5814bfb5e956b9fe39761f8a2180866a3317a5
Ellie <eagyeman1234@gmail.com>
2023-11-23 13:01:08 -0500

    import and add useState and add onChange to input field

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import { useState } from "react"
 import "./styles.css"
 
 function App() {
+  const [newItem, setNewItem] = useState("")
+
+
   return (
     <>
       <form className="new-item-form">
         <div className="form-row">
           <label htmlFor="item">New Item</label>
-          <input type="text" name="" id="item" />
+          <input 
+            value={newItem}
+            onChange={e => setNewItem(e.target.value)}
+            type="text" 
+            id="item" 
+          />
         </div>
         <button className="btn">Add</button>
       </form>
@@ -36,3 +45,5 @@ export default App
 
 
 // when you want to return multiple elements from a component you need to use a fragment <></> and wrap it around everything you want to render in the component. If you don't wrap it in a fragment it can only return one thing example only the form can be returned and not the h1 as well if there was no fragment around it.
+
+// useState is a React hook that allows you to add state to a functional component. It returns an array with two values: the current state and a function to update it.
