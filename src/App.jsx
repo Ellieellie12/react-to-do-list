6078ae92e77a8fbd5afdd5df8d8377716c84ef0f
Ellie <eagyeman1234@gmail.com>
2023-11-25 16:26:08 -0500

    add function for deleting todos

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,26 @@ function App() {
         { id: crypto.randomUUID(), title: newItem, completed: false },
       ]
     })
+    setNewItem("")
   }
 
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if(todo.id === id) {
+          return { ...todo, completed}
+        }
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+
+  }
   return (
     <>
       <form onSubmit={handleSubmit} className="new-item-form">
@@ -32,14 +50,15 @@ function App() {
       </form>
       <h1 className="header">Todo List</h1>
       <ul className="list">
+        {todos.length === 0 && "No Todos"}
         {todos.map(todo => {
           return (
             <li key={todo.id}>
               <label>
-                <input type="checkbox" checked={todo.completed}/>
+                <input type="checkbox" checked={todo.completed} onChange={e => toggleTodo(todo.id, e.target.checked)}/>
                 {todo.title}
               </label>
-              <button className="btn btn-danger"> Delete</button>
+              <button onClick={() => deleteTodo(todo.id)} className="btn btn-danger"> Delete</button>
             </li>
           ) 
         })}
