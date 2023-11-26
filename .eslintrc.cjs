5a5232ed0e427bc6e0733a10b36e9944cc02f98f
Ellie <eagyeman1234@gmail.com>
2023-11-25 19:49:13 -0500

    fix linting error

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  root: true,
+  // root: true,
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
@@ -12,9 +12,10 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
+    'react-refresh/only-export-components': 
       'warn',
-      { allowConstantExport: true },
-    ],
+      'react/prop-types': 'off',
+      'react/no-unescaped-entities': 'off',
+      // { allowConstantExport: true },
   },
 }
